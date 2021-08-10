d2581910e66212378411555ef07f5559fffc7356
ronaldemanuel <emronald15el@gmail.com>
2021-08-10 04:47:24 -0300

    advertising container implemented

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,7 +23,73 @@ class _HomePageState extends State<HomePage> {
           color: Color(0xFF10397B),
           height: 150,
         ),
+        buildAdvertisingContainer(),
       ],
+    );
+  }
+
+  buildAdvertisingContainer() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.purple,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Expanded(child: buildTextContent()),
+          SizedBox(width: 16),
+          Placeholder(
+            color: Colors.white,
+            fallbackHeight: 150,
+            fallbackWidth: 130,
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildTextContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        buildGeneralText(
+          'Top destinos mais buscados',
+          fontSize: 22,
+          color: Colors.white,
+        ),
+        SizedBox(height: 8),
+        buildGeneralText(
+          'Corre que ta rolando muita promoção',
+          fontSize: 16,
+          color: Colors.white,
+        ),
+        SizedBox(height: 8),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xFFF8FF04),
+          ),
+          onPressed: () {},
+          child: buildGeneralText(
+            'EU QUERO!',
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
+    );
+  }
+
+  buildGeneralText(String text,
+      {double? fontSize, Color? color, FontWeight? fontWeight}) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        color: color,
+        fontWeight: fontWeight,
+      ),
     );
   }
 }
